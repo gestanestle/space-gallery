b1ab93e6d7a8174f353052e60cad4fea4ab8100c
gestanestle <gesta.nestleo@gmail.com>
2021-09-08 23:29:20 +0800

    search query and cards done! browse all images next >|

--- space-gallery.js
+++ space-gallery.js
@@ -1,5 +1,6 @@
 let f = document.getElementById("form")
 let x = document.getElementById("search")
+let s2 = document.getElementById("section-two")
 let cards = document.getElementById("section2")
 
 f.addEventListener("submit", (e)=> {
@@ -21,20 +22,24 @@ function remove_data() {
     }   
 }
 
+
 function show_data(item) { 
     document.getElementById("bottom-line").innerHTML = `
-        <div class="row mt-8 bg-primary" style="height:auto; font-style:itallic;">
-            <p style="color: white;">Search results for "${x.value} (${item.length}): </p>
+        <div class="row" id="search-results-number">
+            <p style="color: white;">Search results for "${x.value}" (${item.length}): </p>
         </div>
     `
 
     let i = 0
     while (i < item.length){
-        
+
+
+        globalThis.title = item[i].data[0].title
+
         let update = `
 
-        <div class="col" id="col-cards">
-        <div class="card mt-2" style="width: 18rem;" style="color: black">
+        <div class="col" id="colCard">
+        <div class="card mt-2" style="width: 18rem;" style="color: black" onclick="return showDetails('${item[i].data[0].title}')">
         <img src="${item[i].links[0].href}" class="card-img-top" id="card-img">
         <div class="card-body text-center" style="color: black" id="card-body">
         <p class="card-title" id="card-title">${item[i].data[0].title}</p>
@@ -49,68 +54,60 @@ function show_data(item) {
         i++;
 
    }
-        /** 
-    
-            let item = y[i].data
-            let image = y[i].links[0].href
-            let nasaID = item[i].nasa_id
-            let title = item[i].title
-            let center = item[i].center
-            let date_created = item[i].date_created
-            let desc = item[i].description
-        
- 
-    
-        */
-    
+    location.href = "#section-two"; 
     
 }
 
-cards.addEventListener("click", (e)=> {
-    e.preventDefault()
-    showDetails()
-})
 
-let title = document.getElementById("card-title")
 
-function showDetails (title) {
-
+async function showDetails (title) {
     console.log(title)
-    /**
-     * 
-    const response = await fetch(`https://images-api.nasa.gov/search?title=${title.value}`)
+    
+    const response = await fetch(`https://images-api.nasa.gov/search?title=${title}`)
     const data = await response.json()
-    console.log(data)
-     * <div class="modal-dialog modal-dialog-centered  modal-dialog-scrollable" id="showDetails">
+    const item = data.collection.items
+
+    document.getElementById("modal").innerHTML = `
+    <div class="modal-dialog  modal-dialog-scrollable" id="showDetails">
 
             <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title text-center">${item[i].data[0].title}</h5>
+              <h5 class="modal-title w-100 text-center"  style="color: black">
+              <i class="bi bi-moon-stars"></i>
+              &emsp;
+              ${item[0].data[0].title}
+              &emsp;
+              <i class="bi bi-moon-stars"></i>
+              </h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="card mt-2" style="width: 18rem;" style="color: black">
-            <img src="${item[i].links[0].href}" class="card-img-top" id="card-img">
-            <div class="card-body text-center" style="color: black" id="card-body">
-            <p>${item[i].description}</p>   
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Center: ${item[i].center}</li>
-                <li class="list-group-item">NASA ID: ${item[i].nasa_id}</li>
-                <li class="list-group-item">${item[i].date_created}</li>
+            <div class="modal-body" id="popUp-modal">
+                <div class="card mt-2" style="width: 25rem;" style="color: black">
+            <img src="${item[0].links[0].href}" class="card-img-top" id="card-img">
+            <div class="card-body text-center" style="color: black; height:auto">
+            <p>${item[0].data[0].description}</p>   
+            <ul class="list-group list-group-flush" >
+                <li class="list-group-item">Center: ${item[0].data[0].center}</li>
+                <li class="list-group-item">NASA ID: ${item[0].data[0].nasa_id}</li>
+                <li class="list-group-item">${item[0].data[0].date_created}</li>
             </ul>
             </div>
-                </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
-          </div>
-
             
-            </div>
-     */
+          </div>
+            
+        </div>
+    `
+
+    var thisModal = new bootstrap.Modal(document.getElementById('modal'))
+    thisModal.show();
+
 }
 
-
-
-
+window.onbeforeunload = function() { 
+    window.setTimeout(function () { 
+        window.location = 'index.html';
+    }, 0); 
+    window.onbeforeunload = null;
+}
