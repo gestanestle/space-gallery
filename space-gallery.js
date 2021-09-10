de6e647bcd10123361c67f696103a681a2240236
gestanestle <gesta.nestleo@gmail.com>
2021-09-10 22:15:17 +0800

    done!

--- space-gallery.js
+++ space-gallery.js
@@ -13,35 +13,59 @@ let s3= document.getElementById("section-three")
 let tiles = document.getElementById("section3")
 let nav = document.getElementById("navigation-links")
 
+//section-two
+
+var n = 1;
 
 f.addEventListener("submit", (e)=> {
     e.preventDefault()
-    fetch_data(x.value)
+    fetch_data(x.value, n)
 })
 
-async function fetch_data (y) {
-    const response = await fetch(`https://images-api.nasa.gov/search?q=${y}&media_type=image`)
-    const data = await response.json()
-    remove_data()
-    show_data(data.collection.items)
-}
 
-function remove_data() {
-    var col = document.querySelectorAll("text-center.row.col");
-    if(col != null) {
+function remove_contents() {
+    var navrow = document.querySelectorAll("navigation-links");
+    var titleRow = document.querySelectorAll("all-images");
+    var row = document.querySelectorAll("#search-results-number");
+
+    var con1 = document.querySelectorAll("text-center.row.col");
+    var con2 = document.querySelectorAll(".container-fluid.text-center.row.col");
+
+    if(navrow != null) {
+        nav.innerHTML = "";
+    }
+    if(titleRow != null) {
+        document.getElementById("all-images").innerHTML = "";
+    }
+    if(row != null) {
+        document.getElementById("bottom-line").innerHTML = "";
+    }  
+
+    if(con1 != null) {
         cards.innerHTML = "";
-    }   
-}
-
-function remove_tiles() {
-    var col = document.querySelectorAll(".container-fluid.text-center.row.col");
-    if(col != null) {
+    }
+    
+    if(con2 != null) {
         tiles.innerHTML = "";
-    }   
+    }  
 }
 
+function nextPage() { 
+    n++;
+    fetch_data(x.value, n); 
+}
 
-function show_data(item) { 
+function prevPage() {
+    n--;
+    fetch_data(x.value, n); 
+}
+
+async function fetch_data (y, n) {
+    remove_contents(); 
+    const response = await fetch(`https://images-api.nasa.gov/search?q=${y}&media_type=image&page=${n}`)
+    const data = await response.json()
+    const item = data.collection.items
+
     document.getElementById("bottom-line").innerHTML = `
         <div class="row" id="search-results-number">
             <p style="color: white;">Search results for "${x.value}" (${item.length}): </p>
@@ -51,16 +75,15 @@ function show_data(item) {
     let i = 0
     while (i < item.length){
 
-
         globalThis.title = item[i].data[0].title
 
         let update = `
 
         <div class="col" id="colCard">
-        <div class="card mt-2" style="width: 18rem;" style="color: black" onclick="return showDetails('${item[i].data[0].title}')">
+        <div class="card mt-4 mb-4" style="width: 18rem;" style="color: black" onclick="return showDetails('${item[i].data[0].title}')">
         <img src="${item[i].links[0].href}" class="card-img-top" id="card-img">
         <div class="card-body text-center" style="color: black" id="card-body">
-        <p class="card-title" id="card-title">${item[i].data[0].title}</p>
+        <p class="card-title text-truncate" id="card-title">${item[i].data[0].title}</p>
         </div>
         </div>
         </div>
@@ -68,18 +91,111 @@ function show_data(item) {
         `
 
         cards.insertAdjacentHTML("beforeend", update)
-        console.log(i + ": " + item[i].data[0].title)
         i++;
 
    }
-    location.href = "#section-two"; 
+
+   if (item.length != 0) {
+
+   nav.innerHTML = `
+    <div class="col">
+        <a href="#section-three" id="next" onclick="return nextPage()">Next</a>
+    </div>
+   `
+   }
+   else {
+       cards.innerHTML = `
+       <p id="text">No results found.</p>
+       `
+   }
+   if (n > 1) {
+    let update = `
+    <div class="col">
+        <a href="#section-three" id="prev" onclick="return prevPage()">Previous</a>
+    </div>
+   `
+    if (document.getElementById('prev') == null) {
+        nav.insertAdjacentHTML("afterbegin", update)
+    }
+    }
+
+
+   location.href = "#section-two"
+
+   return n;
     
 }
 
+//section-three
 
+var o = 1;
+
+function next_page() { 
+    o++;
+    allImages(o); 
+}
+
+function prev_page() {
+    o--;
+    allImages(o);
+}
+
+async function allImages (o) {
+    remove_contents(); 
+    const response = await fetch(`https://images-api.nasa.gov/search?media_type=image&page=${o}`)
+    const data = await response.json()
+    const item = data.collection.items
+
+    document.getElementById("all-images").innerHTML = `<h3>All Images from NASA Archive</h3>`
+
+    let i = 0
+    while (i < item.length){
+
+        let update = `
+
+        <div class="col" id="colCard">
+        <div class="card mt-2 mb-2 text-center" style="color: black; width: 9rem;" onclick="return showDetails('${item[i].data[0].title}')">
+        <img src="${item[i].links[0].href}" class="card-img-top" id="card-img-2">
+        <div class="card-body text-center" style="color: black" id="card-body-2">
+        <p class="card-title text-truncate" id="card-title-2" >${item[i].data[0].title}</p>
+        </div>
+        </div>
+        </div>
+        
+        `
+
+        tiles.insertAdjacentHTML("beforeend", update)
+        i++;
+
+   }
+ 
+   if(item.length != 0) {
+   nav.innerHTML = `
+    <div class="col">
+        <a href="#section-three" id="next" onclick="return next_page()">Next</a>
+    </div>
+   `
+    }
+
+   if (o > 1) {
+    let update = `
+    <div class="col">
+        <a href="#section-three" id="prev" onclick="return prev_page()">Previous</a>
+    </div>
+   `
+    if (document.getElementById('prev') == null) {
+        nav.insertAdjacentHTML("afterbegin", update)
+    }
+    }
+
+
+   s3.scrollIntoView();
+
+   return o;
+
+}
 
 async function showDetails (title) {
-    console.log(title)
     
     const response = await fetch(`https://images-api.nasa.gov/search?title=${title}`)
     const data = await response.json()
@@ -122,73 +238,6 @@ async function showDetails (title) {
     thisModal.show();
 
 }
-
-var o = 1;
-
-function next_page() {
-    remove_tiles();  
-    o++;
-    allImages(o); 
-    console.log("To next: " + o)
-}
-
-function prev_page() {
-    remove_tiles();
-    o--;
-    allImages(o);
-    console.log("To previous: " + o)
-}
-
-async function allImages (o) {
-    const response = await fetch(`https://images-api.nasa.gov/search?keywords=hubble&media_type=image&page=${o}`)
-    const data = await response.json()
-    const item = data.collection.items
-
-    let i = 0
-    while (i < item.length){
-
-
-        let update = `
-
-        <div class="col" id="colCard">
-        <div class="card mt-2 text-center" style="color: black; width: 9rem;" onclick="return showDetails('${item[i].data[0].title}')">
-        <img src="${item[i].links[0].href}" class="card-img-top" id="card-img-2">
-        <div class="card-body text-center" style="color: black" id="card-body-2">
-        <p class="card-title" id="card-title-2" >${item[i].data[0].title}</p>
-        </div>
-        </div>
-        </div>
-        
-        `
-
-        tiles.insertAdjacentHTML("beforeend", update)
-        i++;
-
-   }
-
-   nav.innerHTML = `
-    <div class="col">
-        <a href="#section-three" id="next" onclick="return next_page()">Next</a>
-    </div>
-   `
-
-   if (o > 1) {
-    let update = `
-    <div class="col">
-        <a href="#section-three" id="prev" onclick="return prev_page()">Previous</a>
-    </div>
-   `
-    if (document.getElementById('prev') == null) {
-        nav.insertAdjacentHTML("afterbegin", update)
-    }
-    }
-
-   s3.scrollIntoView();
-
-   return o;
-
-}
-
 
 
 
